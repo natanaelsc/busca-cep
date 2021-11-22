abf0877d986cee5f5d9811d2956048b4e720af1d
Natanael Silva <natanael96@live.com>
2021-11-22 17:01:20 -0300

    First Commit

---
+++ src/test/java/br/com/cepapi/CepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cepapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
